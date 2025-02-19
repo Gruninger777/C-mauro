b83fe1e7db313aa9166464bb1ff47af501a866c1
Gruninger777 <fritzgruningerneto@gmail.com>
2025-02-18 22:13:57 -0300

    Add files via upload

---
+++ C moedas cotação.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  float usd = 5.86;
+  float euro = 5.94;
+  float btc = 554924.07;
+  printf("Cotação: %.3f\n", usd);
+  printf("Cotação: %.3f\n", euro);
+  printf("Cotação: %.3f\n", btc);
+  return 0;
+}
